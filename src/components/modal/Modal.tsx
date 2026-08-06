85f99d7b6bc4f92c0194edb71ff7c61d422d188d
camposEduardo48 <oicedu48@gmail.com>
2026-08-06 00:34:34 -0300

    style: update on modal background color

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -10,8 +10,8 @@ interface ModalProps {
 export const Modal = ({ children, onClick, title }: ModalProps) => {
 	const footerDate = new Date()
 	return (
-		<main className="flex z-50 box-border fixed top-0 left-0 bg-black/30 items-center justify-center h-screen w-screen">
-			<section className="flex flex-col box-content rounded-2xl h-auto w-100 items-center bg-white/10 backdrop-blur-3xl border border-white/10 shadow-2xl">
+		<main className="flex z-40 box-border fixed top-0 left-0 bg-black/30 items-center justify-center h-screen w-screen">
+			<section className="flex z-50 inset-0 flex-col box-content rounded-2xl h-auto w-100 items-center bg-slate-800 border border-white/10 shadow-2xl">
 				<header className="flex h-auto w-full p-3 justify-between rounded-tr-2xl rounded-tl-2xl bg-white/20">
 					<h2>{title}</h2>
 					<nav>
